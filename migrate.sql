3e44be16f507c946a724517a9ff09823293e14f3
Mike Partelow <github@mikepartelow.com>
2017-03-12 10:59:24 -0700

    migration

---
+++ migrate.sql
@@ -0,0 +1,7 @@
+drop table buttons;
+create table buttons (
+  id integer primary key autoincrement,
+  name text not null,
+  status text not null,
+  timestamp timestamp not null default current_timestamp
+);
