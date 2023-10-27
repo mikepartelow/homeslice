cb48536582215e7fd6d69cd82f7333e9a94b6155
Mike Partelow <github@mikepartelow.com>
2023-10-26 22:20:07 -0700

    works

--- apps/chime/chime.py
+++ apps/chime/chime.py
@@ -1,82 +1,35 @@
 #!/venv/bin/python
 
-import os
-import sys
 import time
-import socket
-from threading import Thread
-from random import choice
 
-from urllib.parse import quote
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-
-from soco.discovery import by_name, discover
-from soco import snapshot
-
+from soco import snapshot, SoCo
 
 def parse_args():
     """Parse the command line arguments"""
     import argparse
 
-    description = "Play local files with Sonos by running a local web server"
+    description = "Interrupt a Zone, play a media file, then attempt to resume the Zone"
     parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument("zone", help="The name of the zone to play from")
+    parser.add_argument("zone_ip_address")
+    parser.add_argument("media_title")
+    parser.add_argument("media_uri")
+    parser.add_argument("--player-volume", type=int, required=False, default=40)
 
     return parser.parse_args()
-
 
 def main():
     args = parse_args()
     print(
-        " Will use the following settings:\n"
-        " Zone: {args.zone}".format(args=args)
+        " zone_ip_address: {args.zone_ip_address}\n"
+        " media_title: {args.media_title}\n"
+        " media_uri: {args.media_uri}\n"
+        " player_volume: {args.player_volume}".format(args=args)
     )
 
-    zone_names = [zone_.player_name for zone_ in discover()]
-    print(zone_names)
-    print(dir(zone_names[0]))
-
-    # Get the zone
-    zone = by_name(args.zone)
-
-    # Check if a zone by the given name was found
-    if zone is None:
-        zone_names = [zone_.player_name for zone_ in discover()]
-        print(
-            "No Sonos player named '{}'. Player names are {}".format(
-                args.zone, zone_names
-            )
-        )
-        sys.exit(1)
-
-    # Check whether the zone is a coordinator (stand alone zone or
-    # master of a group)
-    if not zone.is_coordinator:
-        print(
-            "The zone '{}' is not a group master, and therefore cannot "
-            "play music. Please use '{}' in stead".format(
-                args.zone, zone.group.coordinator.player_name
-            )
-        )
-        sys.exit(2)
-
-
-    url = "http://192.168.1.5/api/v0/chime/swed.mp3"
-    title = "4:20"
-
-
-# - http: // nucnuc.local/api/v0/chime/swed.mp3 renders the wrong thing!
-# - by_name() seems not to work in container, can init zone elsewise?
-
-
-    # number_in_queue = zone.add_uri_to_queue(url)
-    # # play_from_queue indexes are 0-based
-    # zone.play_from_queue(number_in_queue - 1)
+    zone = SoCo(args.zone_ip_address)
 
     snapshots = []
-
     for member in zone.group.members:
         print(f" member: {member.player_name}")
 
@@ -94,10 +47,12 @@ def main():
                 print(f"  exception: pause {member.player_name}")
     for member in zone.group.members:
         if not member.mute:
-            member.volume = 40
-    zone.play_uri(url, title=title)
+            member.volume = args.player_volume
+
+    zone.play_uri(args.media_uri, title=args.media_title)
 
     time.sleep(6)
+
     for snap in snapshots:
         try:
             snap.restore(fade=False)
--- pulumi/chime/chime.py
+++ pulumi/chime/chime.py
@@ -1,13 +1,14 @@
 import pulumi
 import pulumi_kubernetes as kubernetes
 import homeslice
-from secrets import backup_todoist
+from secrets import chime as chime_secret
 from pathlib import Path
 
 NAME = "chime"
 
 def app(namespace: str, config: pulumi.Config) -> None:
-    # image = config["image"]
+    image = config["image"]
+    chimes = config["chimes"]
     nginx = config["nginx"]
     pvc_mount_path = config["pvc_mount_path"]
     container_port = int(config["container_port"])
@@ -16,6 +17,8 @@ def app(namespace: str, config: pulumi.Config) -> None:
 
     metadata=homeslice.metadata(NAME, namespace)
 
+    # an nginx deployment serves up media from a persistent volume
+    #
     pvc = kubernetes.core.v1.PersistentVolumeClaim(
         NAME,
         metadata=metadata,
@@ -56,8 +59,6 @@ def app(namespace: str, config: pulumi.Config) -> None:
         )
     ]
 
-# kubectl cp /local/path/to/file my-pod:/path/in/container
-
     deployment = homeslice.deployment(NAME,
                                       nginx,
                                       metadata,
@@ -76,3 +77,42 @@ def app(namespace: str, config: pulumi.Config) -> None:
                                     [ingress_prefix],
                                     path_type="ImplementationSpecific",
         )
+
+    # cronjobs schedule the chimes.
+    for chime in chimes:
+        for zone in chime_secret.ZONES:
+            name = make_name(NAME, chime, zone)
+
+            zone_cronjob = kubernetes.batch.v1.CronJob(
+                name,
+                metadata=homeslice.metadata(name, namespace),
+                spec=kubernetes.batch.v1.CronJobSpecArgs(
+                    schedule=chime["schedule"],
+                    job_template=kubernetes.batch.v1.JobTemplateSpecArgs(
+                        spec=kubernetes.batch.v1.JobSpecArgs(
+                            template=kubernetes.core.v1.PodTemplateSpecArgs(
+                                spec=kubernetes.core.v1.PodSpecArgs(
+                                    restart_policy="Never",
+                                    containers=[
+                                        kubernetes.core.v1.ContainerArgs(
+                                            name=name,
+                                            image=image,
+                                            args=[
+                                                zone["ip_address"],
+                                                chime["media_title"],
+                                                chime["media_uri"].replace("{{ingress}}", chime_secret.INGRESS),
+                                            ]
+                                        )
+                                    ]
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+
+def make_name(name: str, chime: dict, zone: dict) -> str:
+    boring_title = chime["media_title"].replace(" ", "-")
+    boring_zone_name = zone["name"].replace(" ", "-")
+    fancy_zone_ip_address = zone["ip_address"].replace('.', '-')
+    return (name+"-"+boring_title+"-"+fancy_zone_ip_address+"-"+boring_zone_name).lower()
