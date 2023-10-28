e41dac072fa33c0db8f20b72f827ac1ded1e8851
Mike Partelow <github@mikepartelow.com>
2023-10-28 10:35:07 -0700

    lint

--- pulumi/backup_todoist/backup_todoist.py
+++ pulumi/backup_todoist/backup_todoist.py
@@ -4,9 +4,9 @@ from pathlib import Path
 import pulumi
 import pulumi_kubernetes as kubernetes
 import homeslice
-from homeslice_secrets import (
+from homeslice_secrets import (  # pylint: disable=no-name-in-module
     backup_todoist as BACKUP_TODOIST_SECRETS,
-)  # pylint: disable=no-name-in-module
+)
 
 NAME = "backup-todoist"
 
--- pulumi/switches/switches.py
+++ pulumi/switches/switches.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import pulumi
 import pulumi_kubernetes as kubernetes
 import homeslice
-from homeslice_secrets import (
+from homeslice_secrets import (  # pylint: disable=no-name-in-module
     switches as SWITCHES_SECRETS,
-)  # pylint: disable=no-name-in-module
+)
 
 NAME = "switches"
 
