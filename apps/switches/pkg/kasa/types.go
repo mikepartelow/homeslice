af1add637b32e09682eb05d864b844a2b43b4646
Mike Partelow <github@mikepartelow.com>
2023-12-12 08:42:53 -0800

    kasa

--- apps/switches/pkg/kasa/types.go
+++ apps/switches/pkg/kasa/types.go
@@ -8,35 +8,11 @@ const (
 )
 
 type LightState struct {
-	OnOff OnOff  `json:"on_off"`
-	Mode  string `json:"mode,omitempty"`
+	OnOff OnOff `json:"on_off"`
 }
 
 type SysInfoResponse struct {
-	ErrCode    int        `json:"err_code,omitempty"`
 	LightState LightState `json:"light_state"`
-	SwVer      string     `json:"sw_ver,omitempty"`
-	HwVer      string     `json:"hw_ver,omitempty"`
-	Type       string     `json:"type,omitempty"`
-	Model      string     `json:"model,omitempty"`
-	Mac        string     `json:"mac,omitempty"`
-	DeviceID   string     `json:"deviceId,omitempty"`
-	HwID       string     `json:"hwId,omitempty"`
-	FwID       string     `json:"fwId,omitempty"`
-	OemID      string     `json:"oemId,omitempty"`
-	Alias      string     `json:"alias,omitempty"`
-	DevName    string     `json:"dev_name,omitempty"`
-	IconHash   string     `json:"icon_hash,omitempty"`
-	RelayState int        `json:"relay_state,omitempty"`
-	OnTime     int        `json:"on_time,omitempty"`
-	ActiveMode string     `json:"active_mode,omitempty"`
-	Feature    string     `json:"feature,omitempty"`
-	Updating   int        `json:"updating,omitempty"`
-	Rssi       int        `json:"rssi,omitempty"`
-	LedOff     int        `json:"led_off,omitempty"`
-	Latitude   int        `json:"latitude,omitempty"`
-	Longitude  int        `json:"longitude,omitempty"`
-	MicType    string     `json:"mic_type,omitempty"`
 }
 
 type SystemResponse struct {
@@ -55,7 +31,7 @@ type SystemRequests struct {
 }
 
 type TransitionLightState struct {
-	OnOff OnOff `json:"on_off"`
+	OnOff OnOff `json:"on_off"` // this can't be "omitempty" because 0 values, like on_off=0, are "empty"
 }
 
 type LightingService struct {
