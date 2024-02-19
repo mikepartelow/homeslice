7ec78afbf260c2195007958ae3db5b72b20abf1f
Mike Partelow <github@mikepartelow.com>
2024-02-19 10:37:02 -0800

    works

--- pulumi/scripts/latest.sh
+++ pulumi/scripts/latest.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-# export CR_USER=mikepartelow
-# export CR_REPO=homeslice
-
 export CR=ghcr.io
+export CR_USER=mikepartelow
+export CR_REPO=homeslice
 export CR_APPS=("backup-todoist" "buttons" "chime" "clocktime" "switches")
 
 for CR_APP in "${CR_APPS[@]}"; do
-    export TAG=$(docker run --rm quay.io/skopeo/stable list-tags --creds $CR_USER:$CR_PAT  docker://ghcr.io/$CR_USER/$CR_REPO/$CR_APP | jq -r '.Tags[] | select(startswith("main."))')
+    export TAG=$(docker run --rm quay.io/skopeo/stable list-tags --creds $CR_USER:$CR_PAT docker://ghcr.io/$CR_USER/$CR_REPO/$CR_APP | jq -r '.Tags[] | select(startswith("main."))')
     export TAGGED_IMAGE="$CR/$CR_USER/$CR_REPO/$CR_APP:$TAG"
-    export SHA256=$(docker manifest inspect $TAGGED_IMAGE | jq -r '.config.digest')
-    export IMAGE=$CR/$CR_USER/$CR_REPO/$CR_APP@$SHA256:$TAG
+    export SHA256=$(docker manifest inspect $TAGGED_IMAGE -v | jq -r '.Descriptor.digest')
+    export IMAGE=$CR/$CR_USER/$CR_REPO/$CR_APP:$TAG@$SHA256
     echo $IMAGE
 done
