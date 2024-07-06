fcd33012e1e9791cb35921a97713f7c6da4f429c
Mike Partelow <github@mikepartelow.com>
2024-07-06 08:59:34 -0700

    revert

--- pulumi/homeslice/metadata.py
+++ pulumi/homeslice/metadata.py
@@ -8,16 +8,14 @@ def metadata(
     name: str,
     labels: dict[str, str] = None,
     annotations: dict[str, str] = None,
-    namespace: str = None,
 ) -> kubernetes.meta.v1.ObjectMetaArgs:
     """THE kubernetes metadata factory"""
 
     labels = labels or {"app.kubernetes.io/name": name}
-    namespace = namespace or homeslice.HOMESLICE
 
     return kubernetes.meta.v1.ObjectMetaArgs(
         annotations=annotations,
         labels=labels,
         name=name,
-        namespace=namespace,
+        namespace=homeslice.HOMESLICE,
     )
