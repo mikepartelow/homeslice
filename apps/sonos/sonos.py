0d3a5c3304565f0f736d5be283bf95ee31b30225
Mike Partelow <github@mikepartelow.com>
2024-08-27 22:35:48 -0700

    neat

--- apps/sonos/sonos.py
+++ apps/sonos/sonos.py
@@ -4,70 +4,87 @@
 import os
 from dataclasses import dataclass
 from http import HTTPStatus
-from soco import SoCo
-import soco.groups
-import soco.core
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 
+from soco import SoCo
+
+
 def getenv_or_raise(name: str) -> str:
+    """Return the value of an environment variable or raise RuntimeError."""
     val = os.environ.get(name)
     if not val:
         raise RuntimeError(f"missing required environment variable: {name}")
     return val
 
-LISTEN_HOST="0.0.0.0"
-LISTEN_PORT=getenv_or_raise("LISTEN_PORT")
-SONOS_IP=getenv_or_raise("SONOS_IP")
+
+LISTEN_HOST = "0.0.0.0"
+LISTEN_PORT = getenv_or_raise("LISTEN_PORT")
+SONOS_IP = getenv_or_raise("SONOS_IP")
+
 
 @dataclass
 class Station:
+    """An internet radio station."""
+
     url: str
     title: str
 
     def play(self):
+        """Play the Station on os.environ[SONOS_IP]"""
         zone = SoCo(SONOS_IP)
         if zone.group:
             for member in zone.group.members:
                 if member.is_coordinator:
                     try:
-                        logger.debug(f"[{member.player_name}].play_uri({self.url})")
-                        member.play_uri(uri=self.url, title=self.title, force_radio=True)
-                    except Exception as e:
+                        logger.debug("[%s].play_uri(%s)", member.player_name, self.url)
+                        member.play_uri(
+                            uri=self.url, title=self.title, force_radio=True
+                        )
+                    except Exception as e:  # pylint:disable=[broad-exception-caught]
                         print(e)
 
 
 STATIONS = {
-    "secret-agent": Station(url="https://somafm.com/m3u/secretagent130.m3u", title="SomaFM Secret Agent (Powered by Kubernetes)")
+    "secret-agent": Station(
+        url="https://somafm.com/m3u/secretagent130.m3u",
+        title="SomaFM Secret Agent (Powered by Kubernetes)",
+    )
 }
 
 logger = logging.getLogger(__name__)
 
 
 class SonosServer(BaseHTTPRequestHandler):
-    def send_ok(self, message:str="OK"):
+    """HTTP server for Sonos control"""
+
+    def send_ok(self, message: str = "OK"):
+        """Send an HTTP 200 OK with an optional HTTP body"""
         self.send_response(HTTPStatus.OK)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(bytes(message, "utf-8"))
 
-    def send_not_found(self, message:str="NOT FOUND"):
+    def send_not_found(self, message: str = "NOT FOUND"):
+        """Send an HTTP 404 Not Found with an optional HTTP body"""
         self.send_response(HTTPStatus.NOT_FOUND)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(bytes(message, "utf-8"))
 
-    def do_GET(self):
-        logging.debug(f"do_GET: {self.path}")
+    def do_GET(self):  # pylint:disable=[invalid-name]
+        """Process HTTP GET"""
+        logging.debug("do_GET: %s", self.path)
         parts = list(map(str.lower, self.path.split("/")))[1:]
-        logger.debug(f"parts={parts}")
 
         if len(parts) > 1 and parts[1] == "on":
             if station := STATIONS.get(parts[0], None):
                 station.play()
-                return self.send_ok("ON")
+                self.send_ok("ON")
+                return
 
         self.send_not_found()
+
 
 def main():
     """ye olde main()"""
@@ -76,7 +93,7 @@ def main():
     logging.getLogger(__name__).setLevel(logging.DEBUG)
 
     server = HTTPServer((LISTEN_HOST, LISTEN_PORT), SonosServer)
-    logging.warning(f"listening on http://{LISTEN_HOST}:{LISTEN_PORT}")
+    logging.warning("listening on http://%s:%s", LISTEN_HOST, LISTEN_PORT)
 
     try:
         server.serve_forever()
--- pulumi/sonos/sonos.py
+++ pulumi/sonos/sonos.py
@@ -36,19 +36,3 @@ def app(config: pulumi.Config) -> None:
             },
         ),
     )
-
-    # {
-    #     "accessory": "Http",
-    #     "name": "Secret Agent",
-    #     "switchHandling": "yes",
-    #     "http_method": "GET",
-    #     "on_url": "http://nucnuc.local/api/v0/sonos/secret-agent/ON",
-    #     "off_url": "http://nucnuc.local/api/v0/sonos/secret-agent/OFF",
-    #     "status_url": "http://nucnuc.local/api/v0/sonos/secret-agent/STATUS",
-    #     "status_on": "ON",
-    #     "status_off": "OFF",
-    #     "service": "Switch",
-    #     "sendimmediately": "",
-    #     "username": "",
-    #     "password": "",
-    # }
