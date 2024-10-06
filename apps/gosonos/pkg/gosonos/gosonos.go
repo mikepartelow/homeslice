9672121137be16ff34d373708aabb1b34a781856
Mike Partelow <github@mikepartelow.com>
2024-10-05 22:07:47 -0700

    ok

--- apps/gosonos/pkg/gosonos/gosonos.go
+++ apps/gosonos/pkg/gosonos/gosonos.go
@@ -51,34 +51,48 @@ func (p *Player) Queue() ([]Track, error) {
 	endpoint := "MediaServer/ContentDirectory/Control"
 	action := "urn:schemas-upnp-org:service:ContentDirectory:1#Browse"
 
-	var queueXML bytes.Buffer
-	err := p.queueXmlTemplate.Execute(&queueXML, struct{ StartingIndex int }{0})
-	if err != nil {
-		return nil, fmt.Errorf("error executing queue XML template: %w", err)
-	}
-
 	logger := p.Logger.With("method", "Queue")
 	logger.Info("", "endpoint", endpoint)
 
-	var dl soap.DidlLite
-	err = p.get(endpoint, action, queueXML.String(), func(r io.Reader) error {
-		return dl.Decode(r)
-	})
-	if err != nil {
-		return nil, fmt.Errorf("error decoding Sonos response: %w", err)
-	}
+	var tracks []Track
+	const pageSize = 100
+	for page := 0; ; page += pageSize {
+		var queueXML bytes.Buffer
+		err := p.queueXmlTemplate.Execute(&queueXML, struct {
+			Page     int
+			PageSize int
+		}{
+			Page:     page,
+			PageSize: pageSize,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("error executing queue XML template: %w", err)
+		}
 
-	tracks := make([]Track, len(dl.Items))
-	for i, item := range dl.Items {
-		// x-sonos-http:track%2f2619614.flac?sid=174&flags=8232&sn=34
-		// FIXME: some if statement to determine this is actually a tidal track
-		uri := item.Res.Value
-		id := strings.Split(strings.Split(uri, "%2f")[1], ".")[0]
-		tracks[i] = Track{
-			TidalTrack: &TidalTrack{
-				Id: id,
-			},
-			URI: uri,
+		var dl soap.DidlLite
+		err = p.get(endpoint, action, queueXML.String(), func(r io.Reader) error {
+			return dl.Decode(r)
+		})
+		if err != nil {
+			return nil, fmt.Errorf("error decoding Sonos response: %w", err)
+		}
+
+		if len(dl.Items) == 0 {
+			break
+		}
+
+		for _, item := range dl.Items {
+			// x-sonos-http:track%2f2619614.flac?sid=174&flags=8232&sn=34
+			// FIXME: some if statement to determine this is actually a tidal track
+			uri := item.Res.Value
+			id := strings.Split(strings.Split(uri, "%2f")[1], ".")[0]
+			track := Track{
+				TidalTrack: &TidalTrack{
+					Id: id,
+				},
+				URI: uri,
+			}
+			tracks = append(tracks, track)
 		}
 	}
 
@@ -88,7 +102,7 @@ func (p *Player) Queue() ([]Track, error) {
 func (p *Player) get(endpoint, action, body string, callback func(io.Reader) error) error {
 	endpoint = fmt.Sprintf("http://%s/%s", p.Address.String(), endpoint)
 	logger := p.Logger.With("method", "get")
-	logger.Info("", "endpoint", endpoint)
+	logger.Debug("", "endpoint", endpoint)
 
 	req, err := http.NewRequest("POST", endpoint, strings.NewReader(body))
 	if err != nil {
