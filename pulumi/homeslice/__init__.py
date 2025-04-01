7a1c559d152772190dcc96b0fb447a53b3620e58
Mike Partelow <github@mikepartelow.com>
2025-03-31 21:51:06 -0700

    pulumi

--- pulumi/homeslice/__init__.py
+++ pulumi/homeslice/__init__.py
@@ -1,17 +1,16 @@
 """kubernetes helpers"""
 
-import configmap
-import cronjob
-import deployment
-import env_from
-import ingress
-import metadata
-import namespace
-import port
-import secret
-import service
-import backup_to_github
-
-__all__ = [ "configmap", "cronjob", "deployment", "env_from", "ingress", "metadata", "namespace", "port", "secret", "service", "backup_to_github", ]
+from .configmap import configmap as configmap
+from .cronjob import cronjob as cronjob
+from .deployment import deployment as deployment
+from .env_from import env_from_configmap as env_from_configmap
+from .env_from import env_from_secret as env_from_secret
+from .ingress import ingress as ingress
+from .metadata import metadata as metadata
+from .namespace import namespace as namespace
+from .port import port as port
+from .secret import secret as secret
+from .service import service as service
+from .backup_to_github import BackupToGithub as BackupToGithub
 
 HOMESLICE = "homeslice"
