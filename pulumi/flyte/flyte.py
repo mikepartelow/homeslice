6a2f2ca0aaa3ab428213f1050dfd550a9fb4d840
Mike Partelow <github@mikepartelow.com>
2025-05-04 23:40:27 -0700

    helm

--- pulumi/flyte/flyte.py
+++ pulumi/flyte/flyte.py
@@ -1,6 +1,7 @@
 """Resources for the homeslice/flyte app."""
 
 import yaml
+import json
 import pulumi_kubernetes as kubernetes
 import pulumi
 import homeslice
@@ -11,27 +12,41 @@ NAME = "flyte"
 
 def app(config: pulumi.Config) -> None:
     """define resources for the homeslice/flyte app"""
-
     ns = homeslice.namespace(config["namespace"])
 
+    db_secret = kubernetes.core.v1.Secret(
+        config["secret-name"],
+        metadata=homeslice.metadata(config["secret-name"], namespace=NAME),
+        type="Opaque",
+        string_data={
+            "202-database-secrets.yaml": json.dumps({
+                "database": {
+                    "postgres": {
+                        "password": "postgres",
+                    }
+                }
+            })
+        },
+    )
+
     for chart in config["charts"]:
-        if filename := config.get("values-resource"):
+        if filename := chart.get("values-resource"):
             with importlib.resources.open_text("flyte", filename) as f:
-                values = yaml.load(f)
+                values = yaml.safe_load(f)
         else:
-            values = config.get("values")
+            values = chart.get("values")
 
         kubernetes.helm.v3.Release(
             chart["name"],
             kubernetes.helm.v3.ReleaseArgs(
-                chart=chart["name"],
+                chart=chart["chart"],
                 name=chart["name"],
                 namespace=config["namespace"],
                 version=chart["version"],
                 repository_opts=kubernetes.helm.v3.RepositoryOptsArgs(
-                    repo=chart["repo"],
+                    repo=chart.get("repo"),
                 ),
                 values=values,
             ),
-            pulumi.ResourceOptions(depends_on=[ns]),
+            pulumi.ResourceOptions(depends_on=[ns,db_secret]),
         )
