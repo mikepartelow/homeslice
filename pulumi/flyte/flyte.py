e4b50deddecd308bfb53684292834d8ebb256ff9
Mike Partelow <github@mikepartelow.com>
2025-05-05 20:39:07 -0700

    helm

--- pulumi/flyte/flyte.py
+++ pulumi/flyte/flyte.py
@@ -6,7 +6,9 @@ import pulumi_kubernetes as kubernetes
 import pulumi
 import homeslice
 import importlib.resources
-
+from homeslice_secrets import (  # pylint: disable=no-name-in-module
+    flyte as FLYTE_SECRETS,
+)
 NAME = "flyte"
 
 
@@ -22,7 +24,7 @@ def app(config: pulumi.Config) -> None:
             "202-database-secrets.yaml": json.dumps({
                 "database": {
                     "postgres": {
-                        "password": "postgres",
+                        "password": FLYTE_SECRETS.DB_PASSWORD,
                     }
                 }
             })
@@ -32,21 +34,41 @@ def app(config: pulumi.Config) -> None:
     for chart in config["charts"]:
         if filename := chart.get("values-resource"):
             with importlib.resources.open_text("flyte", filename) as f:
-                values = yaml.safe_load(f)
+                values = yaml.safe_load(render_values(f.read()))
         else:
-            values = chart.get("values")
+            values = render_values(chart.get("values"))
 
-        kubernetes.helm.v3.Release(
+        transformations = []
+        if chart.get("transform-names"):
+            transformations = [
+                lambda obj: obj["metadata"].update({
+                    "name": obj["metadata"]["name"].replace(f"{chart['name']}-", "", 1)
+                }) if "metadata" in obj and "name" in obj["metadata"] else None
+            ]
+        kubernetes.helm.v3.Chart(
             chart["name"],
-            kubernetes.helm.v3.ReleaseArgs(
+            kubernetes.helm.v3.ChartOpts(
                 chart=chart["chart"],
-                name=chart["name"],
                 namespace=config["namespace"],
                 version=chart["version"],
-                repository_opts=kubernetes.helm.v3.RepositoryOptsArgs(
+                fetch_opts=kubernetes.helm.v3.FetchOpts(
                     repo=chart.get("repo"),
                 ),
                 values=values,
+                transformations=transformations,
             ),
             pulumi.ResourceOptions(depends_on=[ns,db_secret]),
         )
+
+def render_values(thing: str | dict | list) -> str | dict | None:
+    if thing is None:
+        return None
+    if isinstance(thing, dict):
+        for k, v in thing.items():
+            thing[k] = render_values(v)
+    elif isinstance(thing, list):
+        for i in range(0, len(thing)):
+            thing[i] = render_values(thing[i])
+    else:
+        thing = thing.replace("%DB_PASSWORD%", FLYTE_SECRETS.DB_PASSWORD).replace("%MINIO_PASSWORD%", FLYTE_SECRETS.MINIO_PASSWORD)
+    return thing
