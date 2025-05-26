d59fca4cc6b4de9e3f4fc6f0b2fb85bf03eb8cdb
Mike Partelow <github@mikepartelow.com>
2025-05-26 16:06:55 -0700

    working

--- apps/lmz/pkg/lmz/lmz.go
+++ apps/lmz/pkg/lmz/lmz.go
@@ -1,11 +1,11 @@
 package lmz
 
 import (
-	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"log/slog"
-	"mp/lmz/pkg/auth"
 	"mp/lmz/pkg/config"
 	"net/http"
 	"net/url"
@@ -13,125 +13,117 @@ import (
 )
 
 const (
-	lmzGW     = "https://gw-lmz.lamarzocco.io"
-	StatusOff = "StandBy"
-	StatusOn  = "BrewingMode"
+	lmzApiUrl        = "http://lion.lamarzocco.io/api/customer-app/"
+	WidgetCodeStatus = "CMMachineStatus"
+	TokenLifespan    = time.Duration(time.Minute * 10)
 )
 
 // LMZ communicates with a La Marzocco Linea, and possibly other La Marzocco machines.
 type LMZ struct {
-	c          *config.Config
-	logger     *slog.Logger
-	token      string
-	newTokenAt time.Time
+	c            *config.Config
+	newTokenAt   time.Time
+	token        string
+	refreshToken string
 }
 
 // New returns a new LMZ client
-func New(c *config.Config, l *slog.Logger) *LMZ {
+func New(c *config.Config) *LMZ {
 	return &LMZ{
-		c:      c,
-		logger: l,
+		c: c,
 	}
-}
-
-// Status represents
-type Status struct {
-	// Received is the time of the last reported status update
-	Received time.Time `json:"received"`
-	// Machine Status can be "StandBy" or "BrewingMode"
-	MachineStatus string `json:"MACHINE_STATUS"`
 }
 
 // Status returns the status of the machine, or an error.
-func (l *LMZ) Status() (*Status, error) {
+func (l *LMZ) Status() (MachineStatus, error) {
 	if err := l.auth(); err != nil {
-		return nil, err
+		return StatusErr, err
 	}
 
-	endpoint, err := url.JoinPath(lmzGW, fmt.Sprintf("/v1/home/machines/%s/status", l.c.Serial))
+	endpoint, err := url.JoinPath(lmzApiUrl, fmt.Sprintf("things/%s/dashboard", l.c.Serial))
 	if err != nil {
-		return nil, fmt.Errorf("error constructing URL: %w", err)
+		return StatusErr, fmt.Errorf("error constructing URL: %w", err)
 	}
 
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
-		return nil, fmt.Errorf("error creating HTTP request: %w", err)
+		return StatusErr, fmt.Errorf("error creating request for %q: %w", endpoint, err)
 	}
 
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", l.token))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", l.token))
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("error getting status: %w", err)
+		return StatusErr, fmt.Errorf("error getting status: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		panic(fmt.Sprintf("oops: %d", resp.StatusCode))
+		return StatusErr, logError(endpoint, resp)
 	}
 
-	var status struct {
-		Data Status `json:"data"`
-	}
+	var dashboard thingDashboard
 
-	err = json.NewDecoder(resp.Body).Decode(&status)
+	err = json.NewDecoder(resp.Body).Decode(&dashboard)
 	if err != nil {
-		return nil, fmt.Errorf("error decoding response JSON: %w", err)
+		return StatusErr, fmt.Errorf("error decoding response JSON: %w", err)
 	}
 
-	return &status.Data, nil
+	status := StatusErr
+	for _, w := range dashboard.Widgets {
+		if w.Code == WidgetCodeStatus {
+			status = MachineStatus(w.Output.Status)
+			break
+		}
+	}
+	if status == StatusErr {
+		return StatusErr, errors.New("couldn't extract status from response JSON")
+	}
+
+	return status, nil
 }
 
 // TurnOn sets the machine to BrewingMode, AKA "on"
 func (l *LMZ) TurnOn() error {
-	return l.setStatus(StatusOn)
+	return l.setStatus(StatusBrewingMode)
 }
 
 // TurnOff sets the machine to StandBy, AKA "off"
 func (l *LMZ) TurnOff() error {
-	return l.setStatus(StatusOff)
+	return l.setStatus(StatusStandBy)
 }
 
-func (l *LMZ) setStatus(status string) error {
+func (l *LMZ) setStatus(status MachineStatus) error {
 	if err := l.auth(); err != nil {
 		return err
 	}
 
-	endpoint, err := url.JoinPath(lmzGW, fmt.Sprintf("/v1/home/machines/%s/status", l.c.Serial))
+	endpoint, err := url.JoinPath(lmzApiUrl, fmt.Sprintf("things/%s/command/CoffeeMachineChangeMode", l.c.Serial))
 	if err != nil {
 		return fmt.Errorf("error constructing URL: %w", err)
 	}
 
-	var bb = struct {
-		Status string `json:"status"`
-	}{
-		Status: status,
-	}
-
-	body, err := json.Marshal(bb)
+	body, err := newChangeModeBody(status)
 	if err != nil {
-		return fmt.Errorf("error marshaling body JSON: %w", err)
+		return fmt.Errorf("couldn't construct changeModeBody: %w", err)
 	}
 
-	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(body))
+	req, err := http.NewRequest("POST", endpoint, body)
 	if err != nil {
 		return fmt.Errorf("error creating status request: %w", err)
 	}
 
-	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", l.token))
+	req.Header.Set("Content-type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("error posting to status endpoint: %w", err)
+		return fmt.Errorf("error posting to %q: %w", endpoint, err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("API error: %s", resp.Status)
+		return logError(endpoint, resp)
 	}
 
 	return nil
@@ -139,12 +131,58 @@ func (l *LMZ) setStatus(status string) error {
 
 func (l *LMZ) auth() error {
 	if l.token == "" || time.Now().After(l.newTokenAt) {
-		l.logger.Warn("fetching auth token")
-		token, newTokenAt, err := auth.GetToken(l.c)
-		if err != nil {
-			return err
+		slog.Warn("fetching auth token")
+
+		if l.refreshToken != "" {
+			panic("FIXME")
 		}
-		l.token, l.newTokenAt = token, newTokenAt
+
+		return l.signin()
 	}
+
 	return nil
+}
+
+func (l *LMZ) signin() error {
+	endpoint, err := url.JoinPath(lmzApiUrl, "auth/signin")
+	if err != nil {
+		return fmt.Errorf("error constructing URL: %w", err)
+	}
+
+	body, err := newSigninBody(l.c.Auth.Username, l.c.Auth.Password)
+	if err != nil {
+		return fmt.Errorf("error constructing signin body: %w", err)
+	}
+
+	resp, err := http.Post(endpoint, "application/json", body)
+	if err != nil {
+		return fmt.Errorf("error making auth request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return logError(endpoint, resp)
+	}
+
+	var signinResponse signinResponseBody
+
+	err = json.NewDecoder(resp.Body).Decode(&signinResponse)
+	if err != nil {
+		return fmt.Errorf("error decoding auth response JSON: %w", err)
+	}
+	l.token, l.newTokenAt = signinResponse.Token, time.Now().Add(TokenLifespan)
+
+	slog.Info("fetched auth token", "expires_at", l.newTokenAt)
+
+	return nil
+}
+
+func logError(endpoint string, resp *http.Response) error {
+	body, _ := io.ReadAll(resp.Body)
+	slog.Error("error response from status endpoint",
+		"status", resp.Status,
+		"headers", resp.Header,
+		"body", string(body),
+	)
+	return fmt.Errorf("error response from %q: %s", endpoint, resp.Status)
 }
---
+++ apps/lmz/pkg/lmz/model.go
@@ -0,0 +1,80 @@
+package lmz
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+type signinRequestBody struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func newSigninBody(username, password string) (io.Reader, error) {
+	body := signinRequestBody{
+		Username: username,
+		Password: password,
+	}
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling signin request body: %w", err)
+	}
+
+	return bytes.NewReader(bodyBytes), nil
+}
+
+type signinResponseBody struct {
+	Token        string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
+type thingDashboard struct {
+	SerialNumber string            `json:"serialNumber"`
+	Name         string            `json:"name"`
+	Widgets      []dashboardWidget `json:"widgets"`
+}
+
+type dashboardWidget struct {
+	Code   string               `json:"code"`
+	Output *machineStatusWidget `json:"output,omitempty"`
+}
+
+// CMMachineStatus
+type machineStatusWidget struct {
+	Status string `json:"status"`
+}
+
+type MachineStatus string
+
+const (
+	StatusErr         MachineStatus = ""
+	StatusStandBy     MachineStatus = "StandBy"
+	StatusBrewingMode MachineStatus = "BrewingMode"
+	StatusPoweredOn   MachineStatus = "PoweredOn"
+)
+
+func IsOn(status MachineStatus) bool {
+	return status == StatusPoweredOn
+}
+
+func IsOff(status MachineStatus) bool {
+	return status == StatusStandBy
+}
+
+type changeModeBody struct {
+	Mode MachineStatus `json:"mode"`
+}
+
+func newChangeModeBody(status MachineStatus) (io.Reader, error) {
+	body := changeModeBody{
+		Mode: status,
+	}
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling signin request body: %w", err)
+	}
+
+	return bytes.NewReader(bodyBytes), nil
+}
