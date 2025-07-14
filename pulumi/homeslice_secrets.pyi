38c1be12d3780de13fad53f693964587fd711496
Mike Partelow <github@mikepartelow.com>
2025-07-13 21:17:28 -0700

    fix: add stub file for homeslice_secrets module to resolve mypy errors in CI

---
+++ pulumi/homeslice_secrets.pyi
@@ -0,0 +1,13 @@
+"""Stub file for homeslice_secrets module."""
+
+from typing import Any
+
+# Stub declarations for the secret modules
+backup_tidal: Any
+backup_todoist: Any
+chime: Any
+flyte: Any
+lmz: Any
+sonos: Any
+switches: Any
+unifi: Any 
